4dbff76cc2873e996968d4f2ddf9fc7228a306e2
Unknown <rkeisling@basecampcodingacademy.org>
2017-04-10 22:36:15 -0500

    Added test_file.py to try out some code to fix a bug in prices_spidey.py

--- test_file.py
+++ test_file.py
@@ -2,6 +2,9 @@ import json
 import pickle
 
 def generate_needed_urls():
+    """
+    Makes all urls using deckbox information along with all card information (from MTGStocks).
+    """
     all_needed_urls = []
     with open("card_convert.p", 'rb') as fin:
         card_data = pickle.load(fin)
@@ -11,13 +14,57 @@ def generate_needed_urls():
     for each in deckbox_data:
         name = each['name']
         cardset = each['cardset']
-        if cardset == 'Welcome Deck 2016':
-            continue
-        ids = card_data[cardset][name]
-        created_url = "http://www.mtgstocks.com" + ids['card_id']
+        try:
+            set_dict = card_data[cardset]
+        except KeyError:
+            try:
+                set_dict = card_data[fix_set_transitions(cardset)]
+            except KeyError:
+                continue
+        try:
+            card_dict = set_dict[name]
+        except KeyError:
+            try:
+                card_dict = set_dict[fix_card_transitions(name)]
+            except KeyError:
+                continue
+        created_url = "http://www.mtgstocks.com" + card_dict['card_id']
         all_needed_urls.append(created_url)
     return all_needed_urls
 
+
+def fix_set_transitions(cardset):
+    """
+    Changes the set to the MTGStocks variant (which in some cases there isn't one).
+    """
+    set_transfer_dict = {'Welcome Deck 2016': None}
+    if 'Core Set' in cardset:
+        cardset = '{0} ({1}{2})'.format(cardset[:10], cardset[0], cardset[8:10])
+    else:
+        cardset = set_transfer_dict[cardset]
+    return cardset
+
+
+def fix_card_transitions(name):
+    """
+    Changes some minor card spelling to the MTGStocks variant.
+    """
+    card_transfer_dict = {'Aetherspouts': 'AEtherspouts',
+                          'Aether Adept': 'AEther Adept',
+                          'Aether Gale': 'AEther Gale',
+                          'Aether Searcher': 'AEther Searcher',
+                          'Aether Vial': 'AEther Vial',
+                          'Aetherling': 'AEtherling',
+                          'Aethertow': 'AEthertow',
+                          'Scornful Aether-Lich': 'Scornful AEther-Lich',
+                          'Unravel the Aether': 'Unravel the AEther',
+                          'Yet Another Aether Vortex': 'Yet Another AEther Vortex',
+                          'Forest': None,
+                          'Swamp': None,
+                          'Mountain': None,
+                          'Plains': None,
+                          'Island': None}
+    return card_transfer_dict[name]
 
 if __name__ == '__main__':
     print(generate_needed_urls())
