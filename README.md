24d7d9fcfac68b7d4ea337ff463d6aad79dece1c
Unknown <rkeisling@basecampcodingacademy.org>
2017-05-04 11:41:26 -0500

    Didn't commit weeks ago, not sure what I was trying to do

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-mtg_stocks_scrapy
+This project uses the scrapy framework for Python.
+It scrapes my inventory of Magic the Gathering cards from deckbox.org and then scrapes pricing information from mtgstocks.com (which uses TCGPlayer pricing and is updated daily).
+As of 04/13/17, it stores the pricing information in JSON files. The card ids used to build urls for mtgstocks.com and the set/card information (all of it, ever; card_convert.p and card_nums_all.json are both very large files) are stored in pickled files. card_sets.p contains the set ids for mtgstocks.com.
--- mtg_stocks_scrapy/pipelines.py
+++ mtg_stocks_scrapy/pipelines.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import datetime
 import json
+from scrapy.utils.serialize import ScrapyJSONEncoder
+
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
@@ -13,14 +15,19 @@ class MtgStocksScrapyPipeline(object):
 
 
 class JsonWriterPipeline(object):
+    all_dicts = []
 
     def open_spider(self, spider):
+        print("Opening file...")
         date = datetime.datetime.now().strftime('%m_%d_%y')
         self.file = open('{0}.json'.format(date), 'w')
 
     def close_spider(self, spider):
+        print("Closing file...")
+        json.dumps(self.all_dicts, self.file, cls=ScrapyJSONEncoder)
         self.file.close()
 
     def process_item(self, item, spider):
-        json.dumps(item, self.file)
+        print("Processing {0}".format(item['cardname']))
+        self.all_dicts.append(item)
         return item
